d4ef3eaccb353a119e28327d4552f6514f7a8a10
AntoFruch <Antonin>
2025-03-25 16:44:40 +0100

    #it works for 1 float number
    Fix I2C sender to write data starting from the first integer and append a zero to the data list
    
    enleve crochets
    
    Refactor receiveEvent function to use a for loop for reading data and remove unused index variable
    
    MODIF 0:4
    
    D
    
    Fix I2C sender to write data starting from the correct register address
    
    Fix I2C sender to correctly write data by adjusting byte array indexing
    
    Fix I2C sender to convert byte data to integer list before transmission
    
    Refactor I2C sender to write float data directly as bytes, improving data transmission
    
    Enhance I2C receiveEvent function to read and print float values from byte array
    
    changement de la valeur
    
    print addition info
    
    TRUE
    
    tests I2C

---
+++ I2C/testI2C_slave/testI2C_slave.ino
@@ -0,0 +1,48 @@
+#include <Wire.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin(8);                  // Join I2C bus with address #8
+  Wire.onReceive(receiveEvent); // Register receive event
+  Wire.onRequest(requestEvent); // Register request event
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(500);
+
+}
+
+union floatToBytes {
+  byte valueBuffer[4];
+  float valueReading;
+} converter;
+
+
+void receiveEvent(int byteCount){
+  for(uint8_t index = 0; index<byteCount; index++){
+      converter.valueBuffer[index] = Wire.read();
+      // Serial.print(index);
+      // Serial.print(": ");
+      // Serial.println(converter.valueBuffer[index]);
+  }
+  // Serial.println("");
+  // Serial.print("The number is: ");
+  Serial.println(converter.valueReading);
+}
+
+// Function that executes whenever data is requested by master
+void requestEvent(){
+  const char *response = "Hello Master!";
+  Wire.write(response);
+}
+
+
+
+
