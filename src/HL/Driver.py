a17a529de33f642f46efd2e42c3e6170b09a097b
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2025-04-02 18:05:52 +0200

    Fix calculation of angle and speed in Driver class by correcting vector lookups

--- src/HL/Driver.py
+++ src/HL/Driver.py
@@ -28,10 +28,9 @@ class Driver:
         vect_dir, vect_prop = vect[:16], vect[16:]  # split the vector in 2
         vect_dir = softmax(vect_dir)  # distribution de probabilité
         vect_prop = softmax(vect_prop)
-        print("vect_dir =", vect_dir, "vect_prop =", vect_prop)
-        angle = sum(SPEED_LOOKUP*vect_dir)  # moyenne pondérée des angles
+        angle = sum(ANGLE_LOOKUP*vect_dir)  # moyenne pondérée des angles
         # moyenne pondérée des vitesses
-        vitesse = sum(ANGLE_LOOKUP*vect_prop)
+        vitesse = sum(SPEED_LOOKUP*vect_prop)
         return angle, vitesse
 
 
