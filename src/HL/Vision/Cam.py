0ce856c79924334c3bda59accdfe167832d5496a
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2025-04-02 11:45:32 +0200

    Update photo save path to new directory for captured images

--- src/HL/Vision/Cam.py
+++ src/HL/Vision/Cam.py
@@ -5,5 +5,5 @@ home_dir = os.environ['HOME'] #set the location of your home directory
 cam = Camera()
 
 cam.start_preview()
-cam.take_photo(f"{home_dir}/Desktop/new_image.jpg") #save the image to your desktop
+cam.take_photo(f"{home_dir}/CoVAPSy/src/HL/Vision/Captured_image/new_image.jpg") #save the image to your desktop
 cam.stop_preview()
