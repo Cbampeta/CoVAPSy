65cab9c174b548ac3138cc91ab1c91e7521ad893
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2025-04-02 11:50:10 +0200

    Remove camera preview functionality and update image save path

--- src/HL/Vision/Cam.py
+++ src/HL/Vision/Cam.py
@@ -4,6 +4,5 @@ import os
 home_dir = os.environ['HOME'] #set the location of your home directory
 cam = Camera()
 
-cam.start_preview()
 cam.take_photo(f"{home_dir}/CoVAPSy/src/HL/Vision/Captured_image/new_image.jpg") #save the image to your desktop
-cam.stop_preview()
+cam.close() #close the camera
