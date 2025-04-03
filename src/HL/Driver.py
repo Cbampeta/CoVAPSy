20f05b2551c3ea69617506a32be6c56b3fc5ab65
matthiasbienvenu <matthias.bienvenu@telecom-sudparis.eu>
2025-04-03 19:35:18 +0200

    changed parameters

--- src/HL/Driver.py
+++ src/HL/Driver.py
@@ -44,6 +44,8 @@ class Driver:
         vect_dir = softmax(vect_dir)  # distribution de probabilité
         vect_prop = softmax(vect_prop)
 
+        print(vect_dir, vect_prop)
+
         angle = sum(SPEED_LOOKUP*vect_dir)  # moyenne pondérée des angles
         # moyenne pondérée des vitesses
         vitesse = sum(ANGLE_LOOKUP*vect_prop)
