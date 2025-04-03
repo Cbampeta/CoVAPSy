4802af2cf2d7dedf9a119f0332584029ba90bd18
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2025-04-03 22:43:59 +0200

    Refactor main loop in Car.py for improved execution flow

--- src/HL/Car.py
+++ src/HL/Car.py
@@ -210,10 +210,8 @@ if __name__ == '__main__':
         log.info("Initialisation terminée")
         if input("Appuyez sur D pour démarrer ou tout autre touche pour quitter") in ("D", "d") or bp2.is_pressed:
             log.info("Depart")
-            for i in range(10):
-                GR86.turn_around()
-            # while True:
-            #     GR86.main()
+            while True:
+                GR86.main()
         else:
             raise Exception("Le programme a été arrêté par l'utilisateur")
     except KeyboardInterrupt:
